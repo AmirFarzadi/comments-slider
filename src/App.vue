2f4f6b7a439a058550634b0a4202ebe625ccc483
Amir <amir.hossein138020@gmail.com>
2024-10-01 15:11:27 +0330

    Completion of the comments-slider project

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,111 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <div id="headerContainer" class="text-center">
+      <h2 id="header">Customer reviews</h2>
+    </div>
+
+    <div
+      id="siliderTemplate"
+      class="d-flex flex-row mt-5"
+      style="width: 900px; height: 300px"
+    >
+      <div id="leftIcon" class="px-3 d-flex align-items-center">
+        <i class="bi bi-caret-left-fill fs-2" @click="nextBtnHandler"></i>
+      </div>
+      <div id="content">
+        <div
+          id="profileContainer"
+          class="d-flex align-items-center justify-content-center mb-3"
+        >
+          <img
+            :src="showEmployee.profile_picture"
+            alt=""
+            class="rounded-circle border-danger border-4"
+            style="width: 100px; height: 100px"
+          />
+        </div>
+        <h6 id="employeeName" class="text-danger text-center m-0">
+          {{ showEmployee?.name }}
+        </h6>
+        <h5 id="jobTitle" class="text-center my-2">
+          {{ showEmployee.job_title }}
+        </h5>
+        <p id="description" class="mt-3">{{ showEmployee.description }}</p>
+      </div>
+
+      <div id="rightIcon" class="px-3 d-flex align-items-center">
+        <i class="bi bi-caret-right-fill fs-2" @click="previousBtnHandler"></i>
+      </div>
+    </div>
+  </div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
+<script setup>
+import { onMounted, ref } from "vue";
+import Employees from "./data";
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+// const showEmployee = ref(null)
+const showEmployee = ref({
+  id: Employees[0].id,
+  name: Employees[0].name,
+  profile_picture: Employees[0].profile_picture,
+  job_title: Employees[0].job_title,
+  description: Employees[0].description,
+});
+let counter = 0;
+
+function updateEmployeeDisplay (){
+  Employees.forEach((item,index) => {
+    index == counter ? (showEmployee.value = item) : "";
+  });
 }
+function nextBtnHandler() {
+  updateEmployeeDisplay()
+  counter++;
+  counter > Employees.length-1 ? (counter = 0) : "";
+  localStorage.setItem("employee", JSON.stringify(showEmployee.value));
+}
+function previousBtnHandler (){
+  updateEmployeeDisplay()
+  counter--;
+  counter < 0 ? (counter = Employees.length-1) : "";
+  localStorage.setItem("employee", JSON.stringify(showEmployee.value));
+}
+onMounted(()=>{
+  // const item = JSON.parse(localStorage.getItem("employee"))
+  showEmployee.value = JSON.parse(localStorage.getItem("employee"))
+  const index = Employees.findIndex((element)=>{
+     return element.id == showEmployee.value.id
+  })  
+  counter = index
+  
+})
+
+setInterval(()=>{
+  nextBtnHandler()
+},3000)
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+body {
+  background-color: #ffffff;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+#header {
+  position: relative;
+  display: inline;
+}
+#header::after {
+  content: "";
+  position: absolute;
+  width: 105%;
+  height: 4px;
+  background-color: #e84393;
+  bottom: -10px;
+  left: -7px;
+  z-index: 100;
 }
 </style>
---
+++ src/data.js
@@ -0,0 +1,74 @@
+const Employees = [  
+    {  
+        id: "1",  
+        name: "Ali Hosseini",  
+        profile_picture: "https://images.pexels.com/photos/697509/pexels-photo-697509.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",  
+        job_title: "Project Manager",  
+        description: "Ali is responsible for planning, executing, and overseeing projects. He works to manage resources effectively and ensure projects are completed on time and within budget."  
+    },  
+    {  
+        id: "2",  
+        name: "Sanaz Haghighi",  
+        profile_picture: "https://images.pexels.com/photos/415829/pexels-photo-415829.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",  
+        job_title: "Software Developer",  
+        description: "Sanaz designs and programs software applications. She provides technical solutions tailored to user needs through various programming languages."  
+    },  
+    {  
+        id: "3",  
+        name: "Mohammad Reza Ghasemi",  
+        profile_picture: "https://images.pexels.com/photos/1040880/pexels-photo-1040880.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",  
+        job_title: "Digital Marketing Specialist",  
+        description: "Mohammad Reza manages online advertising and marketing activities. He analyzes data to improve campaign effectiveness and increase website traffic."  
+    },  
+    {  
+        id: "4",  
+        name: "Fatemeh Naderi",  
+        profile_picture: "https://images.pexels.com/photos/774909/pexels-photo-774909.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",  
+        job_title: "Graphic Designer",  
+        description: "Fatemeh creates visually appealing images and designs for marketing materials and social media. She uses design software to create visual branding solutions for various clients."  
+    },  
+    {  
+        id: "5",  
+        name: "Amir Mohabbi",  
+        profile_picture: "https://images.pexels.com/photos/2379004/pexels-photo-2379004.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",  
+        job_title: "Data Analyst",  
+        description: "Amir collects and analyzes data to provide insights that improve business performance. He uses analytical software to identify key trends."  
+    },  
+    {  
+        id: "6",  
+        name: "Sara Mousavi",  
+        profile_picture: "https://images.pexels.com/photos/1130626/pexels-photo-1130626.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",  
+        job_title: "Human Resources Manager",  
+        description: "Sara oversees the recruitment and hiring process while managing employee relations. She works to create a positive and productive work environment."  
+    },  
+    {  
+        id: "7",  
+        name: "Keyvan Rezaei",  
+        profile_picture: "https://images.pexels.com/photos/1819483/pexels-photo-1819483.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",  
+        job_title: "Finance Manager",  
+        description: "Keyvan is responsible for financial planning and overseeing the financial transactions of the company. He manages budgets and analyzes finances to ensure organizational health."  
+    },  
+    {  
+        id: "8",  
+        name: "Negar Yousefi",  
+        profile_picture: "https://images.pexels.com/photos/1542085/pexels-photo-1542085.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",  
+        job_title: "Public Relations Specialist",  
+        description: "Negar manages communication with media and oversees the public image of the company. She assists in news releases and crisis management."  
+    },  
+    {  
+        id: "9",  
+        name: "Hamid Reza Saadatmand",  
+        profile_picture: "https://images.pexels.com/photos/6801642/pexels-photo-6801642.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",  
+        job_title: "IT Manager",  
+        description: "Hamid Reza supervises IT infrastructure and ensures system efficiency and data security. He works to ensure technology enhances business improvement."  
+    },  
+    {  
+        id: "10",  
+        name: "Narges Solimani",  
+        profile_picture: "https://images.pexels.com/photos/8993561/pexels-photo-8993561.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",  
+        job_title: "Sales Specialist",  
+        description: "Narges builds relationships with customers to increase sales of products and services. She identifies customer needs and offers the best solutions."  
+    }  
+]
+
+export default Employees
